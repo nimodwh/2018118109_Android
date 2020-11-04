b84e0db5f96e005458ff17179eeb5767b97f6a8b
nimodwh <794479560@qq.com>
2020-11-05 00:36:18 +0800

    第二次作业

---
+++ ActivityText/app/src/main/java/com/example/activitytext/SecondActivity.java
@@ -0,0 +1,32 @@
+package com.example.activitytext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("SecondActivity","onDestroy");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("SecondActivity",this.toString());
+        setContentView(R.layout.second_layout);
+        Button button2=(Button)findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(SecondActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
